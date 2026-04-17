3c9bad0e9567a93ace95b8bbcd045b27624c692b
Dynsai <DynsaiCode@outlook.com>
2026-04-17 15:40:05 +0200

    pymodule01partial

---
+++ Python_Module_01/ex2/ft_plant_growth.py
@@ -0,0 +1,32 @@
+class Plant:
+    def __init__(self, name: str, height: float, age: int) -> None:
+        self.name: str = name
+        self.height: float = height
+        self.age: int = age
+
+    def grow(self) -> None:
+        self.height += 0.5
+
+    def get_old(self) -> None:
+        self.age += 1
+
+    def info(self) -> None:
+        day_word = "day" if self.age == 1 else "days"
+        print(f"{self.name}: {self.height}cm, {self.age} {day_word} old")
+
+
+plant1 = Plant("Rose", 25, 12)
+
+time_to_pass: int = 7
+i: int = 1
+initial_height: float = plant1.height
+print(f"=== Day {i} ===")
+plant1.info()
+while i < time_to_pass:
+    print(f"=== Day {i} ===")
+    plant1.grow()
+    plant1.get_old()
+    plant1.info()
+    i += 1
+total_grow: float = plant1.height - initial_height
+print(f"Growth this week: {total_grow}cm")
